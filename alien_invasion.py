48bfb0a3dc74df4efb5cee2a2dcb84d8f18a692c
QiushuangQian <qiushuangqian@sina.com>
2020-01-16 14:24:41 +0800

    外星人飞船碰撞反应

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,9 +28,10 @@ def run_game():
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         # bullets.update()
         # for bullet in bullets.copy():
